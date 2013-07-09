b60b9337bb076835e77173f7eb6bd85988ee04bd
PeterVranken <peter_vranken@yahoo.de>
2013-07-09 21:08:36 +0000

    Makefile: Callback into application to support application specific settings.   tc14: Implementation continued. Now using LCD shield to display ADC measurement results. Still not complete, input selection by buttons is not yet done

---
+++ code/applications/tc14/tc14.mk
@@ -0,0 +1,26 @@
+# 
+# Makefile for GNU Make 3.81
+#
+# Included makefile fragment, which specifies some application dependent settings.
+#
+# Help on the syntax of this makefile is got at
+# http://www.gnu.org/software/make/manual/make.pdf.
+#
+# Copyright (C) 2013 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# This sample requires the floating point support for the standard I/O library (printf in
+# the first place).
+IO_FLOAT_LIB = 1
